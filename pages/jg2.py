6ee9574ec6df9dc085015f2e309d9300317e1f7d
Jang Soomi <109soomi@naver.com>
2024-06-20 21:43:28 +0900

    Create jg2.py

---
+++ pages/jg2.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.markdown(   # st.markdown()을 이용한 본문 작성
+    """    
+    
+    ### NBA에 관한 세부정보들을 알고 싶다면?
+    - [홈페이지](https://www.nba.com/)
+    - [경기 스케쥴](https://www.nba.com/schedule)
+    - [NBA 뉴스](https://www.nba.com/news)
+
+    ### 선수에 대해 더 자세한 정보를 알고 싶다면?
+    - [선수 스탯](https://www.nba.com/stats/players)
+    - [선수 정보](https://www.nba.com/players)
+    
+    ### NBA 직관 티켓 또는 굿즈 구매처
+    - [티켓 구매처] (https://nbatickets.nba.com/?cid=nba:tickets:institutional:nbacom:domsites:rd)
+    - [NBA 굿즈 스토어] (https://store.nba.com/source/bm-nbacom-hp)
+    - [리그패스 구매처] (https://www.nba.com/watch/league-pass-stream)
+    
+"""
+)
